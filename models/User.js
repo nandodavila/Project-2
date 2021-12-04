ea54d62e3837eb52a3152d092d7fa294ec5da1c7
GLBryan <garrettleebryan@gmail.com>
2021-12-04 10:19:54 -0500

    fixed idiot mistakes :)

--- models/User.js
+++ models/User.js
@@ -1,8 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
-// middleware in brackets later
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init({
   id: {
@@ -30,9 +33,21 @@ User.init({
       len: [8],
     },
   },
+  hooks: {
+    beforeCreate: async (newUserData) => {
+      newUserData.password = await bcrypt.hash(newUserData.password, 10);
+      return newUserData;
+    },
+    beforeUpdate: async (updatedUserData) => {
+      updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+      return updatedUserData;
+    },
+  },
   sequelize,
   timestamps: false,
   freezeTableName: true,
   underscored: true,
   modelName: 'user',
 });
+
+module.exports = User;
