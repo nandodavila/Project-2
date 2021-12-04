d6852b75234f949824ea5cfc2845dbdd764c8d62
GLBryan <garrettleebryan@gmail.com>
2021-12-04 10:26:42 -0500

    UserList

---
+++ models/UserList.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserList extends Model {}
+
+UserList.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  item_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  img_link: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  purchase_link: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: 'user',
+      key: 'id',
+    },
+  },
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'userlist',
+});
+
+module.exports = UserList;
