1ce4688c2557244febd55da96b43cc7278c30176
Cornelia Herman <corneliabherman@gmail.com>
2021-12-08 21:23:11 -0500

    added query selector all and foreach add event listner

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,23 +1,23 @@
 const addItemToList = async (event) => {
-    event.preventDefault();
-    event.stopPropagation();
+  event.preventDefault();
+  event.stopPropagation();
 
-    const id = event.target.getAttribute('data-id');
-  
-    if (item_name && description && purchase_link) {
-      
-      const response = await fetch('/api/item', {
-        method: 'POST',
-        body: JSON.stringify({ item_name, description, img_link, purchase_link}),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to add item.');
-      }
+  const id = event.target.getAttribute('data-id');
+
+  if (item_name && description && purchase_link) {
+
+    const response = await fetch('/api/item', {
+      method: 'POST',
+      body: JSON.stringify({ item_name, description, img_link, purchase_link }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to add item.');
     }
+  }
 };
 
 
@@ -29,26 +29,27 @@ document
   .addEventListener('submit', addItemToList);
 //This is for updating a common list item
 const editForUser = () => {
-    let username = document.querySelector('.username').textContent;
-    fetch(`/api/users/${username}`, {
-        method: 'GET'
-    })
-        //if user is the user that created the entry then show edit button
-        .then(response => response.json())
-        .then(body => {
-            document.querySelectorAll('.common-item').forEach(card => {
-                const cardUserId = card.children[0].id;
-                if (body.id == cardUserId) {
-                    const cardItemId = card.children[1].id;
-                    card.insertAdjacentHTML('beforeEnd', `<button type="button" id="${cardItemId}" class="btn btn-primary btn-lg edit-item">Edit</button>`);
-                }
-            });
-            document
-                .querySelector('.edit-item')
-                .addEventListener('click', (event) =>{
-                    document.location.replace('/updateitem/'+event.target.id);
-                });
+  let username = document.querySelector('.username').textContent;
+  fetch(`/api/users/${username}`, {
+    method: 'GET'
+  })
+    //if user is the user that created the entry then show edit button
+    .then(response => response.json())
+    .then(body => {
+      document.querySelectorAll('.common-item').forEach(card => {
+        const cardUserId = card.children[0].id;
+        if (body.id == cardUserId) {
+          const cardItemId = card.children[1].id;
+          card.insertAdjacentHTML('beforeEnd', `<button type="button" id="${cardItemId}" class="btn btn-primary btn-lg edit-item">Edit</button>`);
+        }
+      });
+      document
+        .querySelectorAll('.edit-item').forEach(item => {
+          item.addEventListener('click', (event) => {
+            document.location.replace('/updateitem/' + event.target.id);
+          })
         });
+    });
 }
 
 editForUser();
