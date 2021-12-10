bfa30e0fb7326ab9db2b03bedde2846d5be1d742
SeanOVeryMac <seanovery1@gmail.com>
2021-12-10 15:17:12 -0500

    actually fixed it this time

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -48,28 +48,27 @@ document
 
 //This is for updating a common list item
 const editForUser = () => {
-    let username = document.querySelector('.username').textContent;
-    fetch(`/api/users/${username}`, {
-        method: 'GET'
-    })
-        //if user is the user that created the entry then show edit button
-        .then(response => response.json())
-        .then(body => {
-            document.querySelectorAll('.common-item').forEach(card => {
-                const cardUserId = card.children[0].id;
-                if (body.id == cardUserId) {
-                    const cardItemId = card.children[1].id;
-                    card.insertAdjacentHTML('beforeEnd', `<button type="button" id="${cardItemId}" class="btn shadow-sm btn-light btn-lg edit-item">Edit</button>`);
-                }
-            });
-            document
-                .querySelector('.edit-item')
-                .addEventListener('click', (event) =>{
-                    document.location.replace('/updateitem/'+event.target.id);
-                });
-
+  let username = document.querySelector('.username').textContent;
+  fetch(`/api/users/${username}`, {
+    method: 'GET'
+  })
+    //if user is the user that created the entry then show edit button
+    .then(response => response.json())
+    .then(body => {
+      document.querySelectorAll('.common-item').forEach(card => {
+        const cardUserId = card.children[0].id;
+        if (body.id == cardUserId) {
+          const cardItemId = card.children[1].id;
+          card.insertAdjacentHTML('beforeEnd', `<button type="button" id="${cardItemId}" class="btn btn-light btn-lg edit-item">Edit</button>`);
+        }
+      });
+      document
+        .querySelectorAll('.edit-item').forEach(item => {
+          item.addEventListener('click', (event) => {
+            document.location.replace('/updateitem/' + event.target.id);
+          })
         });
-    };
-
+    });
+}
 
 editForUser();
